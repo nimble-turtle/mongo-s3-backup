0ca9ab220d62e74bab1a70ecead6fa7b71161e06
ActivistChecklist <contact@activistchecklist.org>
2026-05-07 16:28:31 -0700

    Add ability to run once

--- src/index.ts
+++ src/index.ts
@@ -2,31 +2,45 @@ import 'dotenv/config';
 import { CronJob } from 'cron';
 import { backup } from './backup';
 
-if (!process.env.BACKUP_CRON_SCHEDULE) {
-  throw new Error('Backup cron schedule is not defined.');
-}
-
-const isRunOnStartupEnabled = () => {
-  return process.env.RUN_ON_STARTUP && process.env.RUN_ON_STARTUP === '1';
+const isRunOnceEnabled = () => {
+  return process.env.RUN_ONCE === '1';
 };
 
-const job = new CronJob(process.env.BACKUP_CRON_SCHEDULE, async () => {
-  try {
-    await backup();
-  } catch (error) {
-    console.error('Error while creating backup: ', error);
+const isRunOnStartupEnabled = () => {
+  return process.env.RUN_ON_STARTUP === '1';
+};
+
+if (isRunOnceEnabled()) {
+  console.log('Running backup once as RUN_ONCE is enabled.');
+  backup()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error('Error while creating backup: ', error);
+      process.exit(1);
+    });
+} else {
+  if (!process.env.BACKUP_CRON_SCHEDULE) {
+    throw new Error('Backup cron schedule is not defined.');
   }
-});
 
-job.start();
+  const job = new CronJob(process.env.BACKUP_CRON_SCHEDULE, async () => {
+    try {
+      await backup();
+    } catch (error) {
+      console.error('Error while creating backup: ', error);
+    }
+  });
 
-console.log(
-  `Backup cron scheduler started: ${process.env.BACKUP_CRON_SCHEDULE}`
-);
+  job.start();
 
-if (isRunOnStartupEnabled()) {
   console.log(
-    `Running backup on startup as RUN_ON_STARTUP is enabled: ${process.env.RUN_ON_STARTUP}`
+    `Backup cron scheduler started: ${process.env.BACKUP_CRON_SCHEDULE}`
   );
-  backup();
+
+  if (isRunOnStartupEnabled()) {
+    console.log(
+      `Running backup on startup as RUN_ON_STARTUP is enabled: ${process.env.RUN_ON_STARTUP}`
+    );
+    backup();
+  }
 }
